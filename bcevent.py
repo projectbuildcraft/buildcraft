b34c908f70434ae7310a57bf4f3f50509f29c4b3
William Morriss <wjmelements@gmail.com>
2012-12-16 22:33:23 -0600

    Generalized unit addition to include buildings, upgrades

--- bcevent.py
+++ bcevent.py
@@ -2,7 +2,7 @@ class Event:
 	def __init__(self,name,production_unit,cost,requirements,time,result,args):
 		self.name = name
 		self.cost = cost #(minerals,gas)
-		self.requirements = requirements # set of requirements in addition to cost
+		self.requirements = requirements # list of requirements in addition to cost; has to be a list because archons
 		self.time = time
 		self.result = result # a function to be called that modifies the player's overall state after the event
 		self.args = args # args for result function
@@ -13,7 +13,7 @@ class Event:
 	build_drone = Event("Build Drone",(50,0),None,17,add_unit,'drone')
 	build_scv = Event("Build SCV",(50,0),None,17,add_unit,'scv')
 	build_probe = Event("Build Probe",(50,0),None,17,add_unit,'probe')
-	build_hatchery = Event("Build Hatchery",(300,0),None,100,expand,'hatchery')
-	build_command_center = Event("Build Command Center",(400,0),None,100,expand,'command center')
-	build_nexus = Event("Build Nexus",(400,0),None,100,expand,'nexus')
+	build_hatchery = Event("Build Hatchery",(300,0),None,100,add_unit,'hatchery')
+	build_command_center = Event("Build Command Center",(400,0),None,100,add_unit,'command center')
+	build_nexus = Event("Build Nexus",(400,0),None,100,add_unit,'nexus')
 
