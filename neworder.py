a8920716d8c4f513fc4d840a18084e3faf82767a
Derek Peirce <dtp3@rice.edu>
2013-03-16 20:30:54 -0500

    Working with Tkinter for front-end GUI

---
+++ neworder.py
@@ -0,0 +1,51 @@
+from Tkinter import *
+
+class App:
+
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.master = master
+
+    def get_master(self):
+        return self.master
+
+def new_order():
+    root = Tk()
+    app = App(root)
+    app.w = Label(root, text = "Create a new build order")
+    app.w.pack()
+
+    app.e = Entry(root)
+    app.e.pack()
+
+    modes = ("Terran","Protoss","Zerg")
+
+    app.here = True
+
+    def submit(app):
+        app.proceed = (app.e.get(), app.v.get())
+        app.get_master().destroy()
+
+    def _destroy(event):
+        app.here = False
+
+    root.bind("<Destroy>", _destroy)
+
+    app.v = StringVar()
+    app.v.set('T')
+    for text in modes:
+        b = Radiobutton(root, text=text, variable=app.v, value=text[0])
+        b.pack()
+
+    app.b = Button(root, text="Done", command=lambda:submit(app))
+    app.b.pack()
+
+    app.proceed = ()
+
+    root.mainloop()
+
+    while app.here:
+        pass
+
+    return app.proceed
