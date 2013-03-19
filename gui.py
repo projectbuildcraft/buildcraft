dd0f75e9989ead296c9aa32af3293246b7972e29
Derek Peirce <dtp3@rice.edu>
2013-03-19 01:18:59 -0500

    Added ticks to graphs

--- gui.py
+++ gui.py
@@ -110,40 +110,43 @@ def supply_graph(order):
     create_graph([worker_supply,supply,cap],title='Supply',fill=[True,True,False],colors=['red','blue','green'])
     
 
-def create_graph(data, fill = None, title = '', colors = None, size = (500,400), padding = (50,30,30,30), labels = ('X values','Y values')):
+def create_graph(data = [default_graph()], fill = None, title = '', colors = None, size = (500,400), padding = (50,30,30,30)):
 
     ''' Data: Iterable containing dictionaries mapping x values to y values '''
 
     if fill == None:
         fill = [False]*len(data)
+    elif type(fill) == type(True):
+        fill = [fill]*len(data)
 
     if colors == None:
         colors = default_colors[:len(data)]
 
     width, height = size
     p_top, p_bottom, p_left, p_right = padding
-    x_label, y_label = labels
 
     root = Tk()
 
-    root.wm_title('Supply over Time')
-
+    root.wm_title(title)
+    
     app = App(root)
 
     app.c = Canvas(root, width = width, height = height)
     app.c.grid()
 
-
     app.c.create_rectangle(0,0,width,height,fill='white')
     app.c.create_line(p_left,p_top,p_left,height - p_bottom)
     app.c.create_line(p_left,height - p_bottom,width - p_right,height - p_bottom)
-
     app.c.create_text(width/2,p_top/2,text = title)
-    app.c.create_text(width/2,height - p_bottom/2,text = x_label)
-#    app.c.create_text(p_left/2,height/2,text = y_label,angle = 90)
-    
+
     max_y = max([max(d.values()) for d in data])
-    max_x = max([max(d.keys()) for d in data])
+    max_x = final_x = max([max(d.keys()) for d in data])
+    print max_y
+
+    y_ticks = calculate_ticks(float(max_y) / (max_ticks - 1))
+    max_y = math.ceil(max_y / y_ticks) * y_ticks
+    x_ticks = calculate_ticks(float(max_x) / (max_ticks - 1))
+    max_x = math.ceil(max_x / x_ticks) * x_ticks
     x_scale = float(width - p_left - p_right) / max_x
     y_scale = float(height - p_top - p_bottom) / max_y
 
@@ -153,6 +156,26 @@ def create_graph(data, fill = None, title = '', colors = None, size = (500,400),
     def plot_y(y):
         return height - p_bottom - y * y_scale
 
+    k = 0
+    while k <= max_y:
+        i = int(round(k))
+        print k, i
+        if abs(i - k) < 0.000001 or True:
+            y = plot_y(i)
+            app.c.create_line(p_left - 3,y,p_left,y)
+            app.c.create_text(p_left - 5,y,text = str(i),anchor = E)
+        k += y_ticks
+
+    k = 0
+    while k <= max_x:
+        i = int(round(k))
+        print k, i
+        if abs(i - k) < 0.000001 or True:
+            x = plot_x(i)
+            app.c.create_line(x,height - p_bottom + 3,x,height - p_bottom)
+            app.c.create_text(x,height - p_bottom + 5,text = str(i),anchor = N)
+        k += x_ticks
+
     for i, d in enumerate(data):
         coords = []
         if fill[i]:
@@ -160,12 +183,40 @@ def create_graph(data, fill = None, title = '', colors = None, size = (500,400),
         for k in d.keys():
             coords.append((plot_x(k),plot_y(d[k])))
         if fill[i]:
-            coords.append((plot_x(max_x),plot_y(0)))
+            coords.append((plot_x(final_x),plot_y(0)))
             app.c.create_polygon(coords, fill=colors[i])
         else:
             app.c.create_line(coords, fill=colors[i])
 
     root.mainloop()
+
+def calculate_ticks(v, r = True):
+    exponent = math.floor(math.log10(v))
+    fraction = v / (10 ** exponent)
+
+    if r:
+        if fraction < 1.5:
+            nice = 1
+        elif fraction < 3:
+            nice = 2
+        elif fraction < 7:
+            nice = 5
+        else:
+            nice = 10
+    else:
+        if fraction <= 1:
+            nice = 1
+        elif fraction <= 2:
+            nice = 2
+        elif fraction <= 5:
+            nice = 5
+        else:
+            nice = 10
+
+    return nice * 10**exponent
+
+# http://trollop.org/2011/03/15/algorithm-for-optimal-scaling-on-a-chart-axis/
+
 
 def get_image(src, size = ()):
     image = Image.open('images/'+src)
